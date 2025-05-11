ffe73912edbbc3ec6b78e1847e07e5d7e11756c4
Eduardo Dantas Luna <edudaluna2000@gmail.com>
2025-05-11 09:44:29 -0300

    Using stow and waybar config is nice now

---
+++ README.md
@@ -0,0 +1,13 @@
+# This is my basic Arch(craft) setup
+
+## Things being installed
+
+- hyprland
+- wofi
+- waybar
+- mako
+- neovim
+
+The rest are most programs I use on a daily basis. (Like discord, docker, etc.)
+
+
---
+++ dotfiles/waybar/.config/waybar/scripts/cpu_temp.sh
@@ -0,0 +1,15 @@
+read -r used_mb total_mb <<< $(free -m | awk '/Mem:/ {print $3, $2}')
+
+# Convert to GiB (rounded up)
+used_gb=$(( (used_mb + 1023) / 1024 ))
+total_gb=$(( (total_mb + 1023) / 1024 ))
+ram_usage="${used_gb}/${total_gb} GB"
+
+# CPU usage
+cpu_usage=$(mpstat 1 1 | awk '/Average/ {print 100 - $NF}')
+
+
+# CPU temp rounded
+cpu_temp=$(sensors | awk '/k10temp-pci-00c3/,/^$/' | grep 'Tctl' | awk '{printf("%d°C", $2)}')
+echo "$ram_usage | CPU $cpu_usage% | $cpu_temp"
+
